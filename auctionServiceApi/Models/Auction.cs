b7c02366c6e1599738759bb20a1adda8dc298f95
N-Edens <128368138+N-Edens@users.noreply.github.com>
2024-12-05 17:35:07 +0100

    MongoDBConncetion

--- auctionServiceApi/Models/Auction.cs
+++ auctionServiceApi/Models/Auction.cs
@@ -1,4 +1,4 @@
-namespace Auction
+namespace AuctionService.Models
 {
     using System;
     using System.Collections.Generic;
@@ -7,13 +7,10 @@ namespace Auction
     using System.Text.Json.Serialization;
     using System.Globalization;
 
-    /// <summary>
-    /// A schema for an auction
-    /// </summary>
-    public partial class Coordinate
+    public partial class Auction
     {
         /// <summary>
-        /// MongoDB's auto-generated ID
+        /// MongoDB's auto-generated ID for the auction document
         /// </summary>
         [JsonPropertyName("_id")]
         public string Id { get; set; }
@@ -28,7 +25,7 @@ namespace Auction
         /// List of bids placed on the auction
         /// </summary>
         [JsonPropertyName("Bids")]
-        public List<Bid> Bids { get; set; }
+        public List<BidElement> Bids { get; set; }
 
         /// <summary>
         /// Reference to the item being auctioned
@@ -37,7 +34,7 @@ namespace Auction
         public string ItemId { get; set; }
     }
 
-    public partial class Bid
+    public partial class BidElement
     {
         /// <summary>
         /// Amount of the bid
@@ -46,7 +43,7 @@ namespace Auction
         public double BidAmount { get; set; }
 
         /// <summary>
-        /// User who placed the bid
+        /// The unique identifier for the user who placed the bid
         /// </summary>
         [JsonPropertyName("UserId")]
         public string UserId { get; set; }
