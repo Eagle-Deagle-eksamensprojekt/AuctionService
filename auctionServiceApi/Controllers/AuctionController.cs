f02800f2efc99ac2ee563a61c83320af629c73dc
MikkelHv <mikkelh208@gmail.com>
2024-12-12 10:52:25 +0100

    Versions kald tilføjet

--- auctionServiceApi/Controllers/AuctionController.cs
+++ auctionServiceApi/Controllers/AuctionController.cs
@@ -43,6 +43,28 @@ namespace AuctionServiceAPI.Controllers
         }
 
         /// <summary>
+        /// Hent version af Service
+        /// </summary>
+        [HttpGet("version")]
+        public async Task<Dictionary<string,string>> GetVersion()
+        {
+            var properties = new Dictionary<string, string>();
+            var assembly = typeof(Program).Assembly;
+
+            properties.Add("service", "OrderService");
+            var ver = FileVersionInfo.GetVersionInfo(
+                typeof(Program).Assembly.Location).ProductVersion ?? "N/A";
+            properties.Add("version", ver);
+            
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString() ?? "N/A";
+            properties.Add("ip-address", ipa);
+            
+            return properties;
+        }
+
+        /// <summary>
         /// Hent alle auktioner.
         /// </summary>
         // Hent alle auktioner og cache resultatet
